efc17645aede69a80e58b516ad1f5a1526075db6
qizl <q@qizl.cn>
2017-05-03 23:10:11 +0800

    -

---
+++ WindowsShade/FormMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsShade
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        public static extern long GetWindowLong(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        public static extern long SetWindowLong(IntPtr hwnd, int nIndex, long dwNewLong);
+
+        [DllImport("user32", EntryPoint = "SetLayeredWindowAttributes")]
+        private static extern int SetLayeredWindowAttributes(IntPtr Handle, int crKey, byte bAlpha, int dwFlags);
+
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_TRANSPARENT = 0x20;
+        const int WS_EX_LAYERED = 0x80000;
+        const int LWA_ALPHA = 2;
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            //this.WindowState = FormWindowState.Maximized;
+            this.Location = new Point(-1920, 0);
+            this.Width = 1920 * 2;
+            this.Height = 1080;
+            SetWindowLong(Handle, GWL_EXSTYLE, GetWindowLong(Handle, GWL_EXSTYLE) | WS_EX_TRANSPARENT | WS_EX_LAYERED);
+            SetLayeredWindowAttributes(Handle, 0, 128, LWA_ALPHA);
+        }
+    }
+}
