0b102fcfaadcd8ff07d543675203c842252ead0e
qizl <q@qizl.cn>
2018-08-03 09:10:14 +0800

    -

--- WindowsShade/Views/FormShade.cs
+++ WindowsShade/Views/FormShade.cs
@@ -52,20 +52,24 @@ namespace WindowsShade.Views
         /// <param name="monitors"></param>
         public void AdjustShade(List<Monitor> monitors)
         {
+            // 1.读取屏幕配置
             monitors = monitors.Where(m => m.Enabled).ToList();
             if (!monitors.Any()) return;
             if (!monitors.Any(m => m.IsMain))
                 monitors[0].IsMain = true;
 
+            // 2.提取主显配置
             var mainMonitor = monitors.First(m => m.IsMain); // 主显
             var mainMonitorIndex = monitors.IndexOf(mainMonitor); // 主显位置
             var d = mainMonitor.Resolution.Y - monitors.Max(m => m.Resolution.Y); // 主显与最大高度显示器高度差
 
+            // 3.计算屏幕大小
             int x = mainMonitorIndex == 0 ? 0 : monitors.Take(mainMonitorIndex).Sum(m => m.Resolution.X) * -1;
             int y = d >= 0 ? 0 : d; // 默认用户多显示器配置为底部对齐
             int width = monitors.Sum(m => m.Resolution.X);
             int height = monitors.Max(m => m.Resolution.Y);
 
+            // 4.设置屏幕大小
             var border = 7;
             var title = 40;
             this.Location = new Point(x - border, y - title);
